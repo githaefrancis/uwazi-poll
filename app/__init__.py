75fc474b7f14696fd576f0b8fcfbee00d0ba7b5f
Francis Githae <francis.githae@student.moringaschool.com>
2022-02-15 06:11:48 +0300

    initialize flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,33 @@
 from flask import Flask
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_simplemde import SimpleMDE
+from flask_bootstrap import Bootstrap
+from flask_wtf.csrf import CSRFProtect
+from flask_mail import Mail
 
+db=SQLAlchemy()
+login_manager=LoginManager()
+login_manager.session_protection='strong'
+login_manager.login_view='auth.login'
+
+simple=SimpleMDE()
+bootstrap=Bootstrap()
+csrf=CSRFProtect()
+mail=Mail()
 
 def create_app(config_name):
   app=Flask(__name__)
   app.config.from_object(config_options[config_name])
+  
+  db.init_app(app)
+  login_manager.init_app()
+  simple.init_app()
+  csrf.init_app(app)
+  bootstrap.init_app(app)
+  mail.init_app(app)
+
 
   from .main import main as main_blueprint
   app.register_blueprint(main_blueprint)
