8adf9d96b46b72c9a3e0cb6bf95859afb7f73e28
Francis Githae <francis.githae@student.moringaschool.com>
2022-02-16 17:04:03 +0300

    add logic to create a post

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,8 +1,8 @@
 from re import A
-from flask import redirect, render_template,request
+from flask import redirect, render_template,request,url_for
 from . import admin
-from .forms import ElectionForm
-from ..models import Election
+from .forms import ElectionForm, PostForm
+from ..models import Election, Post
 from datetime import datetime
 
 
@@ -22,8 +22,20 @@ def admin_view():
 
   return render_template('admin/admin.html',election_form=election_form,elections=elections)
 
-@admin.route('/admin/election/<id>/posts')
-def post(id):
+@admin.route('/admin/election/<id>/posts',methods=['GET','POST'])
+def election(id):
   election=Election.query.filter_by(id=id).first()
+  post_form=PostForm()
+  posts=Post.query.filter_by(election=election).all()
 
-  return render_template('admin/post.html',election=election)
+  if request.method=='POST':
+    if post_form.validate_on_submit():
+      title=post_form.title.data
+      new_post=Post(title=title,election=election)
+      new_post.save_post()
+      return redirect(request.referrer)
+
+
+  return render_template('admin/election.html',election=election,post_form=post_form,posts=posts)
+
+
