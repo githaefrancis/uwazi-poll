037ae327c20df3d682bbcd1fd0ac44ece0c3c7bd
Francis Githae <francis.githae@student.moringaschool.com>
2022-02-16 20:35:41 +0300

    add registration form

---
+++ app/auth/forms.py
@@ -0,0 +1,38 @@
+from flask_wtf import FlaskForm
+
+
+from wtforms import StringField,PasswordField,SubmitField,BooleanField
+from wtforms.validators import InputRequired,Email,EqualTo
+
+
+from wtforms import ValidationError
+
+from ..models import User
+
+class RegistrationForm(FlaskForm):
+  name=StringField('Your Name',validators=[InputRequired()])
+  email=StringField('Your Email Address',validators=[InputRequired(),Email()])
+  student_id=StringField('Student id',validators=[InputRequired()])
+  password=PasswordField('Password',validators=[InputRequired(),EqualTo('password_confirm',message='Password must match')])
+  password_confirm=PasswordField('Confirm Password',validators=[InputRequired()])
+  
+  submit=SubmitField('Sign Up')
+
+  def validate_email(self,data_field):
+    if User.query.filter_by(email=data_field.data).first():
+      raise ValidationError('There is an account with that email ')
+
+  def validate_student_id(self,data_field):
+    if User.query.filter_by(student_id=data_field.data).first():
+      raise ValidationError('Student id already registered')
+
+
+class StudentLoginForm(FlaskForm):
+  student_id=StringField('Your Student Id',validators=[InputRequired(),Email()])
+  password=PasswordField('Password',validators=[InputRequired()])
+  submit=SubmitField('Sign In')
+
+class AdminLoginForm(FlaskForm):
+  email=StringField('Your Email Address',validators=[InputRequired(),Email()])
+  password=PasswordField('Password',validators=[InputRequired()])
+  submit=SubmitField('Sign In')
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,17 @@
+from flask import render_template
 from . import auth
-
+from .forms import AdminLoginForm, RegistrationForm, StudentLoginForm
 @auth.route('/login')
 def login():
-  return 'login'
+  login_form=StudentLoginForm()
+  return render_template('auth/login.html',login_form=login_form)
 
+@auth.route('/admin/login')
+def admin_login():
+  login_form=AdminLoginForm()
+  return render_template('auth/adminLogin.html',login_form=login_form)
+
+@auth.route('/register')
+def register():
+  register_form=RegistrationForm()
+  return render_template('auth/register.html',register_form=register_form)
